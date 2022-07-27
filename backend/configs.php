f0f7899f59b3a97a0825b9faf81e845bec54a955
mikael.quadros <mikael.quadros@gmail.com>
2022-07-26 23:44:31 -0300

    Configurações do banco de dados

---
+++ backend/configs.php
@@ -0,0 +1,6 @@
+<?php
+    define("DB_HOST", "database");
+    define("DB_PORT", "3306");
+    define("DB_USERNAME", "userBD");
+    define("DB_PASSWORD", "12345");
+    define("DB_DATABASE_NAME", "movementRanking");
